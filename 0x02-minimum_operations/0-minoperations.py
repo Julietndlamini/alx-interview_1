53a9c4603e7d0f03013baa07f1a09ba30229c385
Juliet Ndlamini <jndlamini3@gmail.com>
2023-10-22 01:08:27 -0700

    “0-minoperations.py”

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
+"""
+Tasks 0: write a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
+
 
 def minOperations(n):
-    if n <= 1:
+    if n == 1:
         return 0
 
-    operations = 0
-    divisor = 2
+    ops = 0
+    h = 1
+    while h < n:
+        if n % h == 0:
+            h_copy = h
+            ops += 1
+        h += h_copy
+        ops += 1
 
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+    if h == n:
+        return ops
+    else:
+        return 0
